ee3333368039336feed717917bbed97edd606a9f
aryanyadav2003 <yadavaryadav13@gmail.com>
2026-01-20 16:17:04 +0530

    radix sort and backtracking scenario based implemented

---
+++ dsa-csharp-practice/scenario-based/PasswordCrackerSimulator/PasswordCracker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductDiscountSort.PasswordCrackerSimulator
+{
+    internal class PasswordCracker:IPassword
+    {
+        private string secretPassword;
+        private Boolean isCracked;
+        public void GenerateString(int length)
+        {
+            char[] current=new char[length];
+            BackTrackGenerate(0, length, current);
+        }
+        private void BackTrackGenerate(int index, int length, char[] current)
+        {
+            if (index == length)
+            {
+                Console.WriteLine(new string(current));
+                return;
+            }
+            for(char ch='a';ch <= 'z'; ch++)
+            {
+                current[index] = ch;
+                BackTrackGenerate(index + 1, length, current);
+            }
+        }
+        public void SetPassword(string password)
+        {
+            secretPassword= password;
+            isCracked = false;
+            Console.WriteLine("Password set successfully");
+
+        }
+        public void DecodePassword()
+        {
+            if (string.IsNullOrEmpty(secretPassword))
+            {
+                Console.WriteLine("Password not set.");
+                return;
+            }
+            char[] attempt = new char[secretPassword.Length];
+            CrackBacktrack(0, attempt);
+            if(!isCracked){
+                Console.WriteLine("Password not cracked");
+            }
+        }
+        private void CrackBacktrack(int index, char[] attempt)
+        {
+            if (isCracked)
+                return;
+            if (index == secretPassword.Length)
+            {
+                string guess = new string(attempt);
+                Console.WriteLine("Trying : " + guess);
+                if (guess == secretPassword)
+                {
+                    Console.WriteLine(" Password Cracked!");
+                    isCracked = true;
+                }
+                return;
+            }
+            for(char ch = 'a'; ch <= 'z'; ch++)
+            {
+                attempt[index] = ch;
+                CrackBacktrack(index + 1, attempt);
+            }
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/PasswordCrackerSimulator/PasswordMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductDiscountSort.PasswordCrackerSimulator
+{
+    class Program
+    {
+        static void Main()
+        {
+            IPassword cracker = new PasswordCracker();
+            while (true)
+            {
+                Console.WriteLine("--- Password Cracker Simulator ---");
+                Console.WriteLine("1. Generate all strings of length n");
+                Console.WriteLine("2. Set Password");
+                Console.WriteLine("3. Decode Password");
+                Console.WriteLine("4. Exit");
+                Console.Write("Enter choice: ");
+
+                int choice = int.Parse(Console.ReadLine());
+
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter length: ");
+                        int n = int.Parse(Console.ReadLine());
+                        cracker.GenerateString(n);
+                        break;
+
+                    case 2:
+
+                        Console.Write("Enter password: ");
+                        string pwd = Console.ReadLine();
+                        cracker.SetPassword(pwd);
+                        break;
+
+                    case 3:
+
+                        cracker.DecodePassword();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Existing");
+                        return;
+
+                    default:
+
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
+        }
+    }
+}
